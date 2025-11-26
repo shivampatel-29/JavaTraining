c85d42183ee804f1d2dd50c522b7a905943deaa4
Shivam Patel <shivampatel2907@gmail.com>
2025-11-26 15:26:07 +0530

    hashmap

---
+++ java training/Mainn.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Mainn{
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(1, 2);
+        map.put(2, 3);
+        System.out.println(map);
+        System.out.println(map.get(1));
+        System.out.println(map.getOrDefault(11, 3));
+        for(int x :map.keySet()){
+            System.out.println(map.get(x));
+        }
+    }
+}
